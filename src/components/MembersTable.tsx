fdffe4fd390686d50d00999bee803739a4a822b8
Azamat951 <sheripboy6@gmail.com>
2026-04-18 15:35:05 +0500

    Some expand changes(part 5)

--- src/components/MembersTable.tsx
+++ src/components/MembersTable.tsx
@@ -34,10 +34,10 @@ const filteredMembers = members.filter((member) =>
           <tr>
             <th className="text-left px-6 py-3 text-sm font-medium">Name</th>
             <th className="text-left px-6 py-3 text-sm font-medium">Phone Number</th>
-            <th className="text-left px-6 py-3 text-sm font-medium md:table-cell">Status</th>
-            <th className="text-left px-6 py-3 text-sm font-medium md:table-cell">Type</th>
-            <th className="text-left px-6 py-3 text-sm font-medium md:table-cell">Expire Time</th>
-            <th className="text-left px-6 py-3 text-sm font-medium md:table-cell">Action</th>
+            <th className="text-left px-6 py-3 text-sm font-medium hidden md:table-cell">Status</th>
+            <th className="text-left px-6 py-3 text-sm font-medium hidden md:table-cell">Type</th>
+            <th className="text-left px-6 py-3 text-sm font-medium hidden md:table-cell">Expire Time</th>
+            <th className="text-left px-6 py-3 text-sm font-medium hidden md:table-cell">Action</th>
           </tr>
         </thead>
 
